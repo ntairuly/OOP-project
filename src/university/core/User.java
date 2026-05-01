db158213eef699f5fc902058a9dc364377adb7ea
Ntairuly <ntairuly@gmail.com>
2026-05-01 15:55:40 +0500

    Language enum rethinking

--- src/university/core/User.java
+++ src/university/core/User.java
@@ -2,22 +2,24 @@ package university.core;
 
 import java.util.List;
 import java.util.Objects;
-import university.models.news.*;
+import java.util.Scanner;
 import university.models.other.*;
+//public abstract class User implements Notifiable 
+public class User {
 
-public abstract class User implements Notifiable {
-
-	private String id;
-	private String firstName;
-	private String lastName;
-	private String email;
+	private String id = "24B03110";
+	private String firstName = "Nurs";
+	private String lastName= "Tair";
+	private String email = "n@gmail.com";
 	private String password;
-	private Language language;
 	private List<Journal> attendanceJournals;
 	//Previous SubsсribedJournals not understandable 
 
+	Scanner input = new Scanner(System.in); 
+
 	public boolean login() {
 		// TODO - implement User.login
+		
 		throw new UnsupportedOperationException();
 	}
 
@@ -31,27 +33,12 @@ public abstract class User implements Notifiable {
 		// Return only necessary fields: id, name, email
 		// Other fields are ignored for better readability and security
 		String name = firstName + " " +  lastName;
-		if (language == Language.KZ) {
-			return String.format(
-    			"Пайдаланушы[id=%s, аты=%s, email=%s]",
-    			id,
-  				name,
-    			email
-			);
-		}
-		if (language == Language.RU) {
-			return String.format(
-  				"Пользователь[id=%s, имя=%s, email=%s]",
-    			id,
-    			name,
-    			email
-			);
-		}
+
 		return String.format(
-			"User[id=%s, name=%s, email=%s]",
-			id,
-			name,
-			email
+    		Language.INSTANCE.get("User.toString"),
+    		id,
+  			name,
+    		email
 		);
 	}
 
